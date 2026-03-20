d552fd46d84e5e964b45bb1338630da2f1cc4091
JustinEarnest <138951009+JustinEarnest@users.noreply.github.com>
2026-03-20 22:29:25 +0530

    Fix the bugs and updated the gitignore

--- ccms_frontend/src/services/api.js
+++ ccms_frontend/src/services/api.js
@@ -1,68 +1,18 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-baseURL: "http://127.0.0.1:8000/api/",
-
+  baseURL: "http://127.0.0.1:8000/api/",
 });
 
-
 API.interceptors.request.use((req) => {
-<<<<<<< HEAD
-
-const token = localStorage.getItem("token");
-
-if (token) {
-
-req.headers.Authorization = `Bearer ${token}`;
-
-}
-
-return req;
-
-=======
   const token = localStorage.getItem("token");
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
->>>>>>> 599eeff (All work done)
 });
 
-
 API.interceptors.response.use(
-<<<<<<< HEAD
-
-(response) => response,
-
-(error) => {
-
-// If we're not on the login page and get a 401/403, clear tokens and redirect
-
-if (
-
-error.response &&
-
-(error.response.status === 401 || error.response.status === 403) &&
-
-!error.config.url.endsWith("login/")
-
-) {
-
-localStorage.removeItem("token");
-
-localStorage.removeItem("role");
-
-window.location.href = "/";
-
-}
-
-return Promise.reject(error);
-
-}
-
-=======
   (response) => response,
   (error) => {
     // If we're not on the login page and get a 401/403, clear tokens and redirect
@@ -77,8 +27,6 @@ return Promise.reject(error);
     }
     return Promise.reject(error);
   }
->>>>>>> 599eeff (All work done)
 );
-
 
 export default API;
