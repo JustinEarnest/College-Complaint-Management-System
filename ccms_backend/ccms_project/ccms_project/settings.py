d17b76528e3d17fb844690d44b62e63d34e7b4d3
JustinEarnest <138951009+JustinEarnest@users.noreply.github.com>
2026-03-21 20:05:31 +0530

    Backend-bug fixed

--- ccms_backend/ccms_project/ccms_project/settings.py
+++ ccms_backend/ccms_project/ccms_project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'ccms_db',
         'USER': 'ccms_user',
         'PASSWORD': 'ccms123',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
